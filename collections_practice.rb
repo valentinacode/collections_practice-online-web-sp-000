64242de55ca34dcee72b5b0173bac0513d465874
Valentina E <tiyahelfrih@gmail.com>
2020-08-07 14:14:02 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,15 +32,7 @@ end
 def find_a(array)
   array.find_all do |word|
     word[0] == "a"
-  end 
-  
-  def sum_array(array)
-  sum = 0
-  array.each do |num|
-    sum+=num
   end
-  sum
-  
   
   
   
