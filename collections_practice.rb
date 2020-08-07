47c9953c5cf672c1ab9a42dc98a6d7c97fb409df
Valentina E <tiyahelfrih@gmail.com>
2020-08-07 14:13:50 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -35,12 +35,11 @@ def find_a(array)
   end 
   
   def sum_array(array)
-    sum = 0 
-    array.each do |num|
-      sum+num
-    end
-    
-
+  sum = 0
+  array.each do |num|
+    sum+=num
+  end
+  sum
   
   
   
